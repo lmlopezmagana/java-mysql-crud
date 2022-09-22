665e71b001e8535a4c89f664df43f92290b6e5e5
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-09-22 19:40:17 +0200

    Docker & SQL

---
+++ .history/sql/schema_20220922192504.sql
@@ -0,0 +1,8 @@
+CREATE TABLE empleado (
+    id_empleado         MEDIUMINT NOT NULL AUTO_INCREMENT,
+    nombre              VARCHAR(100) NOT NULL,
+    apellidos           VARCHAR(100) NOT NULL,
+    fecha_nacimiento    DATE NOT NULL,
+    email               VARCHAR(320),
+    PRIMARY KEY (id_empleado)
+);
